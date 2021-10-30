fe4f34ba602d8e9e413563cdc88cd250056cc6ba
Toan Phan <toanphan@berkeley.edu>
2021-10-30 13:08:25 -0700

    updated page

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get('/db', async (req, res) => {
 })
 
 app.get("/api/hello", (req, res) => {
-  res.json({message: "Hello from Express server :) "});
+  res.json({message: "Hello from server"});
 })
 
 
